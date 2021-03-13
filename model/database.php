dd490e51dec7a73a4ffba92a1222298d1ab5dd68
Matt Johnson <mattandbucs@gmail.com>
2021-03-13 10:26:19 -0600

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,5 +1,5 @@
 <?php
-  $dsn = 'mysql://vkxaglyqxc0zikln:rmahmp82fiyzf6yq@pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/f4gtcks0wb5h6s92;dbname=f4gtcks0wb5h6s92';
+  $dsn = 'mysql:host=pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=f4gtcks0wb5h6s92';
   $username = 'vkxaglyqxc0zikln';
   $password = 'rmahmp82fiyzf6yq';
 
