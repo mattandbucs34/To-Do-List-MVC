614d6dbce3e0295e76004b7f9a9f45c4edf54bf4
Matt Johnson <mattandbucs@gmail.com>
2021-03-13 10:21:26 -0600

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-  $dsn = 'mysql:host=localhost;dbname=todolist';
-  $username = 'root';
-  $password = 'YaZ5iaewp@z2GU5';
+  $dsn = 'mysql:host=pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=f4gtcks0wb5h6s92';
+  $username = 'vkxaglyqxc0zikln';
+  $password = 'rmahmp82fiyzf6yq';
 
   try {
     $db = new PDO($dsn, $username, $password);
